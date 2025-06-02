5194f8f7acdc40bc73e98343c8f1d1ac6428dd9f
Jtyler139 <jtyler0226@gmail.com>
2025-06-02 16:07:59 -0500

    delete contents of public and added test python file

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from textnode import TextNode
 from textnode import TextType
 
 def main():
-    my_text_node = TextNode("This is some anchor text", TextType.LINK_TEXT, "https://www.boot.dev")
+    my_text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(my_text_node)
 
 if __name__ == "__main__":
