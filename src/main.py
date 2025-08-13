183d16f2150e345e9065b230a7f786a97aecd8df
Jtyler139 <jtyler0226@gmail.com>
2025-08-13 15:23:20 -0500

    added recursive generate page function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from copystatic import copy_content_to_dir
-from markdown_blocks import generate_page
+from markdown_blocks import generate_page, generate_pages_recursive
 
 def main():
 
@@ -11,7 +11,9 @@ def main():
 
     copy_content_to_dir("static", "public")
 
-    generate_page("content/index.md", "template.html", "public/index.html")
+    #generate_page("content/index.md", "template.html", "public/index.html")
+
+    generate_pages_recursive("content", "template.html", "public")
 
 if __name__ == "__main__":
     main()
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -164,5 +164,18 @@ def generate_page(from_path, template_path, dest_path):
     directory = os.path.dirname(dest_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
+        
     with open(dest_path, "w") as file:
         file.write(template_path_content)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    list_dir = os.listdir(dir_path_content)
+    for item in list_dir:
+        item_path = os.path.join(dir_path_content, item)
+        dest_item_path = os.path.join(dest_dir_path, item)
+        if os.path.isfile(item_path):
+            if item_path.endswith('.md'):
+                dest_item_path = dest_item_path.replace('.md', '.html')
+            generate_page(item_path, template_path, dest_item_path)
+        else:
+            generate_pages_recursive(item_path, template_path, dest_item_path)
